abe8f176d205125164601e0d308d402fbb5050ca
rafaelthedev <rafaelthedev@protonmail.com>
2025-11-24 17:53:00 +0000

    adiciona mensagem de em construção

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,24 @@ function Home() {
       style={{
         width: "100%",
         paddingTop: "60px", // espaço no topo
-        textAlign: "center", // centraliza horizontalmente
-        paddingLeft: "20px", // pequenas margens laterais (opcional)
-        paddingRight: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center", // centraliza horizontalmente em qualquer tela
+        paddingLeft: "16px",
+        paddingRight: "16px",
+        boxSizing: "border-box", // evita layout quebrado no mobile
       }}
     >
-      <h1> Em Construção </h1>
+      <h1 style={{ textAlign: "center" }}>Em Construção</h1>
 
       <p
         style={{
           maxWidth: "800px",
-          margin: "0 auto",
+          marginTop: "20px",
+          textAlign: "center",
           fontSize: "18px",
           lineHeight: "1.6",
+          wordBreak: "break-word",
         }}
       >
         Eu, o Programador de Rua, a.k.a Rafael, particularmente acredito que a
